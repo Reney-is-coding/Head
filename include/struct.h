03143e74210398ee8c355bd7c836a16c0640ee31
benjamin <benjamin.grout@epitech.eu>
2018-05-22 14:43:40 +0200

    adding things

---
+++ include/struct.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** Head
+** File description:
+** my.h
+*/
+
+typedef struct all_s {
+	int c_handling;
+	int c_bytes;
+	int n_handling;
+	int lines;
+	int q_handling;
+	int v_handling;
+	int error;
+} all_t;
