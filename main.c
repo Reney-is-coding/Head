5ab605752613f733a30c9ddc00c731fe9b1bc1bf
benjamin <benjamin.grout@epitech.eu>
2018-05-22 14:56:50 +0200

    adding fill_things function

--- main.c
+++ main.c
@@ -6,7 +6,11 @@
 */
 
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "include/my.h"
+#include "include/struct.h"
 
 all_t fill_strutct(void)
 {
@@ -14,7 +18,7 @@ all_t fill_strutct(void)
 
 	all.c_handling = 0;
 	all.c_bytes = 0;
-	all.n_handling = 0
+	all.n_handling = 0;
 	all.lines = 10;
 	all.q_handling = 0;
 	all.v_handling = 0;
@@ -25,23 +29,33 @@ all_t fill_strutct(void)
 all_t fill_things(char *arg, char  *follow, all_t all)
 {
 	if (strcmp(arg, "-c") == 0 || strcmp(arg, "--bytes") == 0) {
-		if (!follow) {
+		if (!follow || atoi(follow) == 0) {
 			all.error = 84;
 			return (all);
 		}
-
+		all.c_bytes = atoi(follow);
+		all.c_handling = 1;
+		all.n_handling = 0;
 	}
-	if (strcmp(arg, "-c") == 0 || strcmp(arg, "--bytes") == 0)
-
+	if (strcmp(arg, "-n") == 0 || strcmp(arg, "--lines") == 0) {
+		if (!follow || atoi(follow) == 0) {
+			all.error = 84;
+			return (all);
+		}
+		all.lines = atoi(follow);
+		all.n_handling = 1;
+		all.c_handling = 0;
+	}
+	return (all);
 }
 
 
 int	main(int ac, char **av)
 {
-	all_t all = fill_strutct;
+	all_t all = fill_strutct();
 	for (int i = 0; av[i]; i++) {
-		fill_things(av[i], av[i+1]);
+		all = fill_things(av[i], av[i+1], all);
 	}
-
+	printf("%d\n", all.lines);
 	return (0);
 }
